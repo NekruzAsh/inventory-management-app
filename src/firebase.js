23d78637cd4b14dcbceb2356d1c49a72998e569c
NekruzAsh <nekruzashrapov16@gmail.com>
2024-07-30 18:27:03 -0400

    update

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyCkg_dltl5uWa97INBHmqlO7SRB2KKG-oo",
+  authDomain: "inventory-management-bb643.firebaseapp.com",
+  projectId: "inventory-management-bb643",
+  storageBucket: "inventory-management-bb643.appspot.com",
+  messagingSenderId: "1035665933481",
+  appId: "1:1035665933481:web:cbb759b7b25246026a15b8",
+  measurementId: "G-ETDWBD5F9B",
 };
 
 // Initialize Firebase
